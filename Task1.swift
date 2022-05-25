095618ebc4b298dba21dd60753f222b3f68eac0c
kipolad <kipolad@bk.ru>
2022-05-26 09:38:44 +1100

    edit task1

--- Task1.swift
+++ Task1.swift
@@ -1,33 +1,13 @@
-
 func printMonth(_ monthNum: Int) {
-    switch monthNum {
-    case 1:
-        print("Январь")
-    case 2:
-        print("Февраль")
-    case 3:
-        print("Март")
-    case 4:
-        print("Апрель")
-    case 5:
-        print("Май")
-    case 6:
-        print("Июнь")
-    case 7:
-        print("Июль")
-    case 8:
-        print("Август")
-    case 9:
-        print("Сентябрь")
-    case 10:
-        print("Октябрь")
-    case 11:
-        print("Ноябрь")
-    case 12:
-        print("Декабрь")
-    default:
+    if monthNum == 12 || monthNum == 1 || monthNum == 2 {
+        print("Зима")
+    } else if monthNum > 2 && monthNum < 6 {
+        print("Весна")
+    } else if monthNum > 5 && monthNum < 9 {
+        print("Лето")
+    } else if monthNum > 8 && monthNum < 12 {
+        print("Осень")
+    } else {
         print("Нет такого месяца")
     }
 }
-
-printMonth(7)
